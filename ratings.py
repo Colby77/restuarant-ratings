5d5b1fba19c783efdf619466be813927d99583d5
Colby Workman <cgw2319@gmail.com>
2021-12-08 09:39:22 -0700

    gets scores.txt lines in a list

--- ratings.py
+++ ratings.py
@@ -3,4 +3,21 @@
 
 # put your code here
 f = open('scores.txt', 'r')
-print(f.read())
+
+restuarants = []
+
+for line in f:
+    new_line = line.split(':')
+    restuarants.append({'name':f'{new_line[0]}', 'rating': f'{new_line[1]}'})
+    # for word in new_line:
+    #     word = word.replace('\n', '2')
+    # print(new_line)
+
+for r in restuarants:
+    # print(f'{r['name']}', f'{r['rating']}', sep=':')
+    print(r['name'] + ':' + r['rating'])
+
+# x = restuarants.items()
+# print(x)
+# print(restuarants)
+# print(f.read())
