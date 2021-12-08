4d57c351dfe3bf3f74221d935b44f8d3b44b6175
Colby Workman <cgw2319@gmail.com>
2021-12-08 10:49:06 -0700

    promts user for a restuarant to add and then displays all restuarants and ratings

--- ratings.py
+++ ratings.py
@@ -2,6 +2,8 @@
 
 
 # put your code here
+from os import read
+
 f = open('scores.txt', 'r')
 
 restuarant_dict = {
@@ -10,19 +12,19 @@ restuarant_dict = {
 
 for line in f:
     new_line = line.split(':')
-    # print(new_line)
     new_line[1] = new_line[1].strip()
-    # print(new_line)
-    # new_line = line.split(':')
-    # print(new_line)
     restuarant_dict['restuarants'].append({'name':f'{new_line[0]}', 'rating': f'{new_line[1]}'})
-    # print(new_line)
 
+new_rest = input('Have a restuarant to add? (y/n) ')
+
+if new_rest == 'y':
+    rest_name = input('Enter the restuarant name: ')
+    rest_rating = input('Enter your rating for the restuarant: ')
+    restuarant_dict['restuarants'].append({'name':f'{rest_name}', 'rating': f'{rest_rating}'})
+else:
+    print('Here are the restuarants and their ratings:\n')
+
+restuarant_dict['restuarants'] = sorted(restuarant_dict['restuarants'], key=lambda k: k['name'])
 
 for r in restuarant_dict['restuarants']:
-    print(r.items())
-
-# x = restuarant_dict['restuarants'].items()
-# print(x)
-# print(restuarants)
-# print(f.read())
+    print(f"{r['name']} is rated at {r['rating']}")
