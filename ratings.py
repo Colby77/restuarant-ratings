ec3c6fcce1278520deed00229a46e67b64214346
Colby Workman <cgw2319@gmail.com>
2021-12-08 15:08:49 -0700

    deleted commented code

--- ratings.py
+++ ratings.py
@@ -2,25 +2,61 @@
 
 
 # put your code here
-# from os import read
 import os
 import sys
 import random
 path = ''
 dirs = os.listdir(path)
 
-
-
-# f = open('scores.txt', 'r')
-
 restuarant_dict = {
     'restuarants' : []
 }
 
-# for line in f:
-#     new_line = line.split(':')
-#     new_line[1] = new_line[1].strip()
-#     restuarant_dict['restuarants'].append({'name':f'{new_line[0]}', 'rating': float(f'{new_line[1]}')})
+def select_file():
+    for file in dirs:
+        if file.endswith('.txt'):
+            print(file)
+    print('Welcome to the Python Rating Program!')
+    choice = input('Select a file to use or type "other" to upload a file:\n')
+    if choice == 'other':
+        path = input('Enter the path of the file to use:\n')
+        f = open(f'{path}', 'r')
+    else:
+        f = open(f'{choice}', 'r')
+    for line in f:
+        new_line = line.split(':')
+        new_line[1] = new_line[1].strip()
+        restuarant_dict['restuarants'].append({'name':f'{new_line[0]}', 'rating': float(f'{new_line[1]}')})
+    print(f'{choice} selected')
+    print('')
+    user_choices()
+
+def user_choices():
+    print('Welcome to the Restuarant Rater!')
+    choice = input('''Would you like to: 
+    (s) See all restuarants & ratings
+    (a) Add a new restuarant
+    (u) Update random restuarant's rating
+    (q) Quit \n''')
+    if choice == 's':
+        seeRatings()
+    elif choice == 'a':
+        getRest()
+    elif choice == 'q':
+        quit()
+    elif choice == 'u':
+        updateRating()
+    else:
+        print('Must pick (s), (a), or (q)\n')
+        user_choices()
+
+def seeRatings():
+    restuarant_dict['restuarants'] = sorted(restuarant_dict['restuarants'], key=lambda k: k['name'])
+    print('Here are the restuarants and their ratings:\n')
+    for r in restuarant_dict['restuarants']:
+        print(f"{r['name']} is rated at {r['rating']}")
+    print('')
+    user_choices()
 
 def getRest():
     name = input('Enter the restuarant name: ').capitalize()
@@ -46,13 +82,6 @@ def createRest(name, rating):
     print('Restuarant Added! \n')
     user_choices()
 
-def seeRatings():
-    restuarant_dict['restuarants'] = sorted(restuarant_dict['restuarants'], key=lambda k: k['name'])
-    print('Here are the restuarants and their ratings:\n')
-    for r in restuarant_dict['restuarants']:
-        print(f"{r['name']} is rated at {r['rating']}")
-    print('')
-    user_choices()
 
 def updateRating():
     for r in restuarant_dict['restuarants']:
@@ -68,41 +97,4 @@ def updateRating():
     print('Success\n')
     user_choices()
 
-def user_choices():
-    print('Welcome to the Restuarant Rater!')
-    choice = input('''Would you like to: 
-    (s) See all restuarants & ratings
-    (a) Add a new restuarant
-    (u) Update random restuarant's rating
-    (q) Quit \n''')
-    if choice == 's':
-        seeRatings()
-    elif choice == 'a':
-        getRest()
-    elif choice == 'q':
-        quit()
-    elif choice == 'u':
-        updateRating()
-    else:
-        print('Must pick (s), (a), or (q)\n')
-        user_choices()
-
-def select_file():
-    for file in dirs:
-        if file.endswith('.txt'):
-            print(file)
-    print('Welcome to the Python Rating Program!')
-    choice = input('Select a file to use or type "other" to upload a file:\n')
-    if choice == 'other':
-        path = input('Enter the path of the file to use:\n')
-        f = open(f'{path}', 'r')
-    else:
-        f = open(f'{choice}', 'r')
-    for line in f:
-        new_line = line.split(':')
-        new_line[1] = new_line[1].strip()
-        restuarant_dict['restuarants'].append({'name':f'{new_line[0]}', 'rating': float(f'{new_line[1]}')})
-    print(f'{choice} selected')
-    print('')
-    user_choices()
 select_file()
